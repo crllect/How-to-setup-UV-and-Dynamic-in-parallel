bd6b7d44004f1c239aaa1b142ba1a2ff8827240e
Crllect <144494583+crllect@users.noreply.github.com>
2024-01-31 00:04:09 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# How-to-setup-UV-and-Dynamic-in-parallel
+ A step by step tutorial on how to setup, integrate and deploy both UltraViolet and Dynamic web proxies simultaniously
---
+++ public/example.js
@@ -0,0 +1,63 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleButton = document.getElementById("toggleUVEnabled");
+
+    toggleButton.addEventListener("click", function () {
+        let UVEnabled = JSON.parse(localStorage.getItem("UVEnabled")) || false;
+
+        UVEnabled = !UVEnabled;
+
+        toggleButton.textContent = UVEnabled
+            ? "Dynamic: OFF"
+            : "Dynamic: ON";
+
+        localStorage.setItem("UVEnabled", JSON.stringify(UVEnabled));
+    });
+
+    let initialUVEnabled =
+        JSON.parse(localStorage.getItem("UVEnabled")) || false;
+    toggleButton.textContent = initialUVEnabled
+        ? "Dynamic: OFF"
+        : "Dynamic: ON";
+});
+
+document // makes it so you can press enter to submit as opposed to just being able to press a button
+    .getElementById("urlInput")
+    .addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            document.getElementById("searchButton").click();
+        }
+    });
+
+document.getElementById("searchButton").onclick = function (event) {
+    event.preventDefault();
+
+    let url = document.getElementById("urlInput").value; // If no periods are detected in the input, search google instead
+    let searchUrl = "https://www.google.com/search?q=";
+
+    if (!url.includes(".")) {
+        url = searchUrl + encodeURIComponent(url);
+    } else {
+        if (!url.startsWith("http://") && !url.startsWith("https://")) { // if no http or https is detected, add https automatically
+            url = "https://" + url;
+        }
+    }
+
+    if (UVEnabled) {
+        iframeWindow.src = __uv$config.prefix + __uv$config.encodeUrl(url);
+    } else {
+        iframeWindow.src =
+            __dynamic$config.prefix +
+            "route?url=" +
+            encodeURIComponent(url);
+    }
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+    var savedEngine = localStorage.getItem("preferredSearchEngine");
+    if (savedEngine) {
+        document.getElementById("searchEngineSelect").value = savedEngine;
+    }
+});
+
+let UVEnabled = JSON.parse(localStorage.getItem("UVEnabled")) || false;
